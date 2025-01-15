ce9955bcbde65eeeb0d15f6086b9c6fc8d9fe1ec
william.stone <will@williamstone.me>
2025-01-15 15:53:52 -0500

    Barebones app

---
+++ src/main/java/com/perspectrix/market/MarketEvaluationReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.perspectrix.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketEvaluationReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketEvaluationReportServiceApplication.class, args);
+	}
+
+}
