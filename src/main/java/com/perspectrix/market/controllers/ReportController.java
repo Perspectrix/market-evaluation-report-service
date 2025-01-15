ac50bf936c8e5969e9d2d28a0ee545b24235af82
william.stone <will@williamstone.me>
2025-01-15 15:55:16 -0500

    Adding in services and controllers

---
+++ src/main/java/com/perspectrix/market/controllers/ReportController.java
@@ -0,0 +1,15 @@
+package com.perspectrix.market.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/report")
+public class ReportController {
+
+    @PostMapping("/generate-report")
+    public String generateReport() {
+
+    }
+}
---
+++ src/main/java/com/perspectrix/market/services/PersonService.java
@@ -0,0 +1,23 @@
+package com.perspectrix.market.services;
+
+import com.perspectrix.market.domain.Person;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@Slf4j
+public class PersonService {
+    private final WebClient webClient;
+
+    public PersonService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+    public Person getPerson(String id) {
+        return webClient.get()
+                .uri("/api/persons/{id}", id)
+                .retrieve()
+                .bodyToMono(Person.class)
+                .block();
+    }
+}
